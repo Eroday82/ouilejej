c2cdf2acb05eb600f72431ff2aabc8c175ed2c07
raph82200 <47741112+raph82200@users.noreply.github.com>
2019-07-20 19:53:03 +0200

    Modif Raph général

---
+++ Assets/Scripts/ControllerPlayer.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ControllerPlayer : MonoBehaviour
+{
+
+    // Vitesse de déplacement 
+    public float vitesseDeplacement;
+
+    // Vitesse de saut
+    public float vitesseDeSaut;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void WalkHandler()
+    {
+        // Input on x (Horizontal)
+        float hAxis = Input.GetAxis("Horizontal");
+
+        // Input on z (Vertical)
+        float vAxis = Input.GetAxis("Vertical");
+
+        // Movement vector
+        Vector2 movement = new Vector2(hAxis * vitesseDeplacement * Time.deltaTime, 0);
+
+        // Calculate the new position
+        Vector2 newPos = transform.position + movement;
+
+        // Move
+        rb.MovePosition(newPos);
+
+        //check that we are moving
+        if (hAxis != 0 || vAxis != 0)
+        {
+            //movement direction
+            Vector3 direction = new Vector3(hAxis, 0, vAxis);
+
+            //option1 : modify the transform
+            //transform.forward = direction;
+
+            //option 2: using our rigid body
+            rb.rotation = Quaternion.LookRotation(direction);
+
+        }
+    }
+
+}
