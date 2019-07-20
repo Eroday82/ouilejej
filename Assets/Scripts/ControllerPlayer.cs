0969fc86a13a5fae2864b57e36201d942bd7c492
raph82200 <47741112+raph82200@users.noreply.github.com>
2019-07-21 01:50:28 +0200

    Saut potable

--- Assets/Scripts/ControllerPlayer.cs
+++ Assets/Scripts/ControllerPlayer.cs
@@ -22,36 +22,49 @@ public class ControllerPlayer : MonoBehaviour
     {
         Déplacement();
         Saut();
+        MeilleurSaut();
     }
 
     void Déplacement()
     {
         float hAxis = Input.GetAxis("Horizontal");
-        Vector2 movement = new Vector2(hAxis * vitesseDeplacement * Time.deltaTime, 0);
-        Vector2 newPos = (Vector2) transform.position + movement;
-        rb.MovePosition(newPos);
+        rb.velocity = new Vector2(hAxis * vitesseDeplacement, rb.velocity.y);
     }
     
     void Saut()
     {
+        
         if (Input.GetButtonDown("Jump"))
         {
-            rb.velocity = Vector2.up * puissanceDeSaut;
-            Vector2 jumpVector = new Vector2(0, Input.GetAxis("Jump") * puissanceDeSaut);
-            rb.AddForce(jumpVector);
+            if (!isJump)
+            {
+                isJump = true;
+                Vector2 jumpVector = new Vector2(0, Input.GetAxis("Jump") * puissanceDeSaut);
+
+                rb.AddForce(jumpVector);
+            }
         }
     }
 
-    //void MeilleurSaut()
-    //{
-    //    if (rb.velocity.y < 0)
-    //    {
-    //        rb.velocity += Vector2.up * Physics2D.gravity.y * (fallMultiplier - 1) * Time.deltaTime;
-    //    }
-    //    else if (rb.velocity.y > 0 && !Input.GetButton("Jump"))
-    //    {
-    //        rb.velocity += Vector2.up * Physics2D.gravity.y * (lowJumpMultiplier - 1) * Time.deltaTime;
+    void MeilleurSaut()
+    {
+        if (rb.velocity.y < 0)
+        {
+            rb.velocity += Vector2.up * Physics2D.gravity.y * (fallMultiplier - 1) * Time.deltaTime;
+        }
+        else if (rb.velocity.y > 0 && !Input.GetButton("Jump"))
+        {
+            rb.velocity += Vector2.up * Physics2D.gravity.y * (lowJumpMultiplier - 1) * Time.deltaTime;
 
-    //    }
-    //}
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Sol"))
+        {
+            isJump = false;
+            rb.velocity = Vector2.zero;
+        }
+    }
 }
