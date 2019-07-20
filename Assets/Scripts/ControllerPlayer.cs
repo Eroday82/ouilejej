38d0754dc446084906d6e7cb4d22e04c0cd1d3a8
raph82200 <47741112+raph82200@users.noreply.github.com>
2019-07-20 20:36:10 +0200

    yeaqh

--- Assets/Scripts/ControllerPlayer.cs
+++ Assets/Scripts/ControllerPlayer.cs
@@ -31,38 +31,14 @@ public class ControllerPlayer : MonoBehaviour
         // Input on x (Horizontal)
         float hAxis = Input.GetAxis("Horizontal");
 
-        // Input on z (Vertical)
-        float vAxis = Input.GetAxis("Vertical");
-
         // Movement vector
         Vector2 movement = new Vector2(hAxis * vitesseDeplacement * Time.deltaTime, 0);
 
         // Calculate the new position
-<<<<<<< HEAD
         Vector2 newPos = (Vector2) transform.position + movement;
 
         // Déplacement
         rb.MovePosition(newPos);
-=======
-        //Vector2 newPos = transform.position + movement;
-
-        // Move
-        //rb.MovePosition(newPos);
-
-        //check that we are moving
-        if (hAxis != 0 || vAxis != 0)
-        {
-            //movement direction
-            Vector3 direction = new Vector3(hAxis, 0, vAxis);
-
-            //option1 : modify the transform
-            //transform.forward = direction;
-
-            //option 2: using our rigid body
-            rb.rotation = Quaternion.LookRotation(direction);
-
-        }
->>>>>>> 55b2dd5c9230eeb7e82f2e30c99bbab7d01292c1
     }
 
 }
